f8f5cf5dbe8ea0348ab257bff6a3bd9a5ae56bc8
Omer Cohen <omcl97@gmail.com>
2025-07-08 20:17:59 +0300

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import './App.css'
-import Pages from "@/pages/index.jsx"
-import { Toaster } from "@/components/ui/toaster"
+import "./App.css";
+import Pages from "@/pages/index.jsx";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <>
+      {/* Hidden netlify forms to help detection */}
+      <div hidden>
+        <form name="ContactHome" data-netlify="true">
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <input type="tel" name="phone" />
+          <textarea name="message"></textarea>
+        </form>
+        <form name="VintageWorkshop" data-netlify="true">
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <input type="tel" name="phone" />
+          <textarea name="message"></textarea>
+        </form>
+        <form name="ProWorkshop" data-netlify="true">
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <input type="tel" name="phone" />
+          <textarea name="message"></textarea>
+        </form>
+      </div>
+
       <Pages />
       <Toaster />
     </>
-  )
+  );
 }
 
-export default App 
+export default App;
