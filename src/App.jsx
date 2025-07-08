380516cc3255602215ddd8923ecdfd300e566985
Omer Cohen <omcl97@gmail.com>
2025-07-08 20:20:48 +0300

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,6 @@ import { Toaster } from "@/components/ui/toaster";
 function App() {
   return (
     <>
-      {/* Hidden netlify forms to help detection */}
-      <div hidden>
-        <form name="ContactHome" data-netlify="true">
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-          <input type="tel" name="phone" />
-          <textarea name="message"></textarea>
-        </form>
-        <form name="VintageWorkshop" data-netlify="true">
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-          <input type="tel" name="phone" />
-          <textarea name="message"></textarea>
-        </form>
-        <form name="ProWorkshop" data-netlify="true">
-          <input type="text" name="name" />
-          <input type="email" name="email" />
-          <input type="tel" name="phone" />
-          <textarea name="message"></textarea>
-        </form>
-      </div>
-
       <Pages />
       <Toaster />
     </>
