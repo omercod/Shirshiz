aeb25791312d4a7e98119789e827b17c4a4946f5
Omer Cohen <omcl97@gmail.com>
2025-07-19 21:26:39 +0300

    SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+/* global fbq */
 import "./App.css";
 import Pages from "@/pages/index.jsx";
 import { Toaster } from "@/components/ui/toaster";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(
+      window,
+      document,
+      "script",
+      "https://connect.facebook.net/en_US/fbevents.js"
+    );
+
+    fbq("init", "631825806347007");
+    fbq("track", "PageView");
+  }, []);
+
   return (
     <>
       <Pages />
